8e8b03d57f9dd49f7d133161b896f21d58e30e14
ameliakiara <lintanggaming24@gmail.com>
2021-12-02 21:25:05 +0700

    ❌ Delete some stuff

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,5 @@ moment.locale('id');
 moment.tz.setDefault(`Asia/Jakarta`);
 
 import client from './client';
-/** Music Dashboard Coming Soon */
-// import Logger from "./classes/Logger";
-// client.http.listen(3000, () => {
-//     Logger.log("SUCCESS", `Web Server has been started!`);
-// });
 client.mongo.connect();
 client.login(client.config.token)
